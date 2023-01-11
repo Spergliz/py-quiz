f2a2986912c658cad9db08e2235ab7a27bd407c3
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2023-01-11 14:49:18 -0700

    Update quizV1.py

--- quizV1.py
+++ quizV1.py
@@ -1,18 +1,37 @@
 
 #score
-int(score = 0)
+score = 0
 
 #title
 print("welcome to my quiz")
 #Q1
-Question1 = input("why do people use linux  ")
-if Question1 == "tux" or "Tux":
+Question1 = input("why do people use linux: ")
+if Question1.lower() == "tux":
     print("correct")
     score = score + 1
 else: 
     print("wrong")
     
-    #Q2 
-    Question2 = input("what is 3+2")
-    if Question2 == 5 or "five": 
-        print
+#Q2 
+Question2 = input("what is 3+2: ")
+if Question2.lower() == 5 or "five": 
+    print ("Correct")
+    score = score + 1
+else:
+     print("wrong")
+
+#Q3
+Question3 = input("what is the first name of the Father of Linux: ")
+if Question3.lower() == "linus":
+    print("correct")
+    score = score + 1
+else:
+    print("wrong")
+
+#Q4
+Question4 = input(": ")
+if Question4.lower() == "":
+    print("correct")
+    score = score + 1
+else:
+    print("wrong")
