3aa9f1eeb9cff823cc5a01cf157ba3d4b040db55
Lizzy Taylor Stromsmoe <p.Stromsmoe@gmail.com>
2023-01-11 14:07:02 -0700

    Update quizV1.py

--- quizV1.py
+++ quizV1.py
@@ -2,5 +2,14 @@
 #score
 score = 0 
 
+#title
+print("welcome to my quiz")
 #Q1
-Question1 = input("")
+Question1 = input("why do people use linux  ")
+if Question1 == "tux" or "Tux":
+    print("correct")
+    score = score + 1
+else: 
+    print("wrong")
+    
+    #Q2
